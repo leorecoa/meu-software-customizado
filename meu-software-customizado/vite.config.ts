2d1bad4f42c4e31153db7f59542ea15c0b27bf33
leorecoa <leorecoa2@gmail.com>
2026-01-05 00:39:56 -0300

    fix: Adiciona index.html e corrige build

--- meu-software-customizado/vite.config.ts
+++ meu-software-customizado/vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
-  root: resolve(__dirname),
   plugins: [react()],
   build: {
     outDir: 'dist',
